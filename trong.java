eaf1589883cf26b4262eb08fc0701f9466145509
Phonglatui <119600393+phong1221@users.noreply.github.com>
2023-10-13 10:31:39 +0700

    Create trong.java

---
+++ trong.java
@@ -0,0 +1,31 @@
+class phanso {
+    private int tuso;
+    private int mauso;
+    public phanso()
+    {
+        tuso=1;
+        mauso=1;
+    }
+    public phanso( int tuso ,int mauso)
+    {
+        this.tuso=tuso;
+        this.mauso=mauso;
+    }
+    public void setTuso(int tuso)
+    {
+        this.tuso =tuso;
+    }
+    public int getTuso()
+    {
+        return tuso;
+    }
+    public void setMauso(int mauso)
+    {
+        this.tuso =tuso;
+    }
+    public int getMauso()
+    {
+        return mauso;
+    }
+    
+}
